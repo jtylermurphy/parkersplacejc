1b7497a7f337a9703f7e0d4f09d497d2f97d6c00
jtylermurphy <jtylermurphy@gmail.com>
2021-10-08 17:48:24 -0500

    update parker place design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Lato', 'helvetica', 'arial', 'sans-serif'],
+      serif: ['Prata', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
