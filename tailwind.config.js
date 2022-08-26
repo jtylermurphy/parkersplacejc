db0816ebeba274d2df4da3c4fd81a87d479f17b5
jtylermurphy <jtylermurphy@gmail.com>
2022-08-26 16:30:19 -0500

    update with carriage house

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./dist/**/*.{html,js}"],
+  media: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       sans: ['Lato', 'helvetica', 'arial', 'sans-serif'],
       serif: ['Prata', 'serif'],
     },
-    extend: {},
+
+    extend: {
+      colors: {
+        gold: {
+            50: '#F6F3EB',
+            100: '#F0EADD',
+            200: '#E4D8C0',
+            300: '#D8C6A4',
+            400: '#CBB587',
+            500: '#BFA36B',
+            600: '#A98949',
+            700: '#826938',
+            800: '#5B4927',
+            900: '#332A16'
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-  purge: [ '_site/**/*.html' ],
+},
 }
